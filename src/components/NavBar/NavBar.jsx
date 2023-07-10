8026e0ad729468efaebd946a4f353ee0ac6ddb44
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2023-07-10 12:05:14 +0400

    deleting extras

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,9 @@
 import { useProduct } from "../../Providers/productProviders";
+
 import styles from "./navBar.module.css";
 
 const NavBar = () => {
   const products = useProduct();
-  // const totalItems = products.filter((p) => p.quantity > 0).length;
-  // const totalItems = products.length;
   const totalItems = products ? products.length : 0;
 
   return (
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,34 +1,13 @@
-import { useContext, useEffect } from "react";
 import { BiTrash } from "react-icons/bi";
-// import { UserContext, WebContext } from "../../App";
 
 import styles from "./product.module.css";
 
-const Product = ({
-  product,
-  onChange,
-  onDecrement,
-  onIncrement,
-  onDelete,
-  // title,
-}) => {
-  // const user = useContext(UserContext);
-  // console.log(user);
-
-  // const userWeb = useContext(WebContext);
-  // console.log(userWeb);
-
+const Product = ({ product, onChange, onDecrement, onIncrement, onDelete }) => {
   return (
     <div className={styles.product}>
       <p>Product name : {product.title}</p>
       <p>Product price: {product.price}</p>
       <span className={styles.value}>{product.quantity}</span>
-      {/* <input
-        className={styles.input}
-        type="text"
-        onChange={onChange}
-        value={product.title}
-      /> */}
       <button
         className={`${styles.button} ${styles.increment}`}
         onClick={onIncrement}
@@ -41,7 +20,6 @@ const Product = ({
           product.quantity === 1 && styles.remove
         }`}
       >
-        {/* {product.quantity <=1 ? <BiTrash/>} */}
         {product.quantity <= 1 ? <BiTrash /> : "-"}
       </button>
       <button onClick={onDelete} className={styles.button}>
